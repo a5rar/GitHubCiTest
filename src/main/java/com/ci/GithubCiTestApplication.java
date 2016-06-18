55c380a9065243471d15b5d4c30e1ec43f5e28c5
Asrar <asrar@asrar-mac.local>
2016-06-18 16:05:09 +0100

    first check in

---
+++ src/main/java/com/ci/GithubCiTestApplication.java
@@ -0,0 +1,12 @@
+package com.ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCiTestApplication.class, args);
+	}
+}
